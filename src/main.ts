31bf08533cbf6006e76d6cb19e56a5579b661b8f
lu <919041098@qq.com>
2021-03-19 23:09:17 +0800

    搭建HTML结构

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
+import {createRouter,createWebHashHistory} from 'vue-router'
+const router = createRouter({
+  history:createWebHashHistory(),
+  routes:[
+    {path:"/",component:Home},
+    {path:'/Doc',component:Doc}
+  ]
+})
+const app =createApp(App)
+app.use(router)
+app.mount('#app')
