56fb994e2778ae3e6c9542eb69f991517c890ba9
lu <919041098@qq.com>
2021-03-20 20:00:10 +0800

    整体布局css

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <top-nav/>
+  <div class="layout">
+    <top-nav class="nav"/>
     <div class="content">
       <aside v-if="asideVisible">
         <h2>组件列表</h2>
@@ -27,33 +27,70 @@
 </template>
 <script lang="ts">
 import TopNav from '../components/TopNav.vue';
-import {inject,Ref} from 'vue'
+import {inject, Ref} from 'vue';
+
 export default {
   components: {TopNav},
-  setup(){
-    const asideVisible = inject<Ref<boolean>>('asideVisible')
-    return {asideVisible}
+  setup() {
+    const asideVisible = inject<Ref<boolean>>('asideVisible');
+    return {asideVisible};
   }
 };
 </script>
 <style lang="scss" scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+
+  > .nav {
+    flex-shrink: 0;
+  }
+
+  > .content {
+    flex-grow: 1;
+    padding-top: 60px;
+    padding-left: 156px;
+    @media (max-width: 500px) {
+      padding-left: 0;
+    }
+  }
+}
+
+.content {
+  display: flex;
+
+  > aside {
+    flex-shrink: 0;
+  }
+
+  > main {
+    flex-grow: 1;
+    padding: 16px;
+    background: lightgreen;
+  }
+}
+
 aside {
   background: #ece5e5;
   width: 150px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  height: 100%;
 
   > h2 {
     margin-bottom: 4px;
   }
-  >ol{
-    >li{
-      padding:4px 0;
+
+  > ol {
+    > li {
+      padding: 4px 0;
     }
   }
-  @media (max-width: 500px){
-    padding:70px 16px;
-    position: fixed;
-    top: 0;
-    left: 0;
+  main{
+    overflow: auto;
   }
 }
 
