bc1f5e07c0881cb1163789439e8a8c1112f9b492
lu <919041098@qq.com>
2021-03-20 15:19:11 +0800

    Home的css样式

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <div class="topNav">
-      <div class="logo"></div>
-      <div class="menu"></div>
-    </div>
+    <top-nav/>
     <div class="content">
       <aside>边框</aside>
       <main>主内容</main>
     </div>
   </div>
 </template>
+<script lang="ts">
+import TopNav from '../components/TopNav.vue';
+
+export default {
+  components: {TopNav}
+};
+</script>
+<style lang="ts"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,46 @@
 <template>
   <div>
-    <div class="topNav">
-      <div class="logo"></div>
-      <div class="menu"></div>
-    </div>
+    <top-nav/>
     <div class="banner">
       <h1>wheel UI</h1>
       <h2>一个厉害的UI框架</h2>
       <p class="actions">
         <a href="">GitHub</a>
+
         <a href="">开始</a>
       </p>
     </div>
   </div>
 </template>
+<script lang="ts">
+import TopNav from "../components/TopNav.vue";
+
+export default {
+  components: {TopNav}
+};
+</script>
 <style lang="scss" scoped>
-.topNav{
-  border: 1px solid red;
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: #f3e9e9;
+
+  > .actions {
+    margin: 8px 0;
+
+    a {
+      margin: 0 8px;
+      background: #ffffff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
 }
 </style>
