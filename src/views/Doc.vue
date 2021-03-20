19226945c114a7132c7605753dd521821d2e8762
lu <919041098@qq.com>
2021-03-20 15:47:06 +0800

    aside样式

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -5,10 +5,18 @@
       <aside>
         <h2>组件列表</h2>
         <ol>
-          <li><router-link to="/doc/switch">Switch 组件</router-link></li>
-          <li><router-link to="/doc/button">Button 组件</router-link></li>
-          <li><router-link to="/doc/dialog">Dialog 组件</router-link></li>
-          <li><router-link to="/doc/tabs">Tabs 组件</router-link></li>
+          <li>
+            <router-link to="/doc/switch">Switch 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs 组件</router-link>
+          </li>
         </ol>
       </aside>
       <main>主内容</main>
@@ -22,4 +30,21 @@ export default {
   components: {TopNav}
 };
 </script>
-<style lang="ts"></style>
+<style lang="scss" scoped>
+aside {
+  background: #ece5e5;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  >ol{
+    >li{
+      padding:4px 0;
+    }
+  }
+}
+</style>
