9074b253481f843dd1b505dab6676bb7d80ddeec
lu <919041098@qq.com>
2021-03-21 09:45:56 +0800

    跟新Switch样式

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -24,28 +24,35 @@ button {
   height: $h;
   width: $h*2;
   border: none;
-  background: grey;
+  background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
-  transition: left 250ms;
+  transition: all 250ms;
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+  }
+  &.checked {
+    background: #1890ff;
+  }
+  &:hover > span {
+    left: calc(100% - #{$h2} - 2px);
+  }
+  &:focus {
+    outline: none;
+  }
+  &:active{
+    > span {width: $h2 + 4px;}
+  }
+  &.checked:active{
+    > span {width: $h2 + 4px; margin-left: -4px;}
+  }
 }
 
-button.checked {
-  background: blue;
-}
-button:hover > span {
-  left: calc(100% - #{$h2} - 2px);
-}
-button:focus {
-  outline: none;
-}
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2 / 2;
-}
+
 </style>
