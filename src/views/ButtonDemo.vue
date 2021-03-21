9fc8f74969d765066647a5ced5ed281e55df27fe
lu <919041098@qq.com>
2021-03-21 13:20:10 +0800

    设置size的css

--- src/views/ButtonDemo.vue
+++ src/views/ButtonDemo.vue
@@ -4,8 +4,24 @@
   <div>
     <Button>按钮一</Button>
     <Button theme="button">按钮二</Button>
-    <Button theme="button">按钮三</Button>
-    <Button theme="button">按钮四</Button>
+    <Button theme="link">按钮三</Button>
+    <Button theme="text">按钮四</Button>
+  </div>
+  <h1>示例二</h1>
+  <div>
+    <Button size="big">大大大</Button>
+    <Button>普普通</Button>
+    <Button size="small">小小小</Button>
+  </div>
+  <div>
+    <Button theme="link" size="big">大大大</Button>
+    <Button theme="link">普普通</Button>
+    <Button size="small" theme="link">小小小</Button>
+  </div>
+  <div>
+    <Button size="big" theme="text">大大大</Button>
+    <Button theme="text">普普通</Button>
+    <Button size="small" theme="text">小小小</Button>
   </div>
 </template>
 
