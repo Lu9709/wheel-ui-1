420648663a8843b27d1e7dd3c8db07b9abe6908a
lu <919041098@qq.com>
2021-03-21 18:28:42 +0800

    创建Tab和Tabs组件

--- src/views/TabsDemo.vue
+++ src/views/TabsDemo.vue
@@ -1,109 +1,19 @@
 <template>
-Tabs 的内容
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
-  <p>Tabs content</p>
+  <div>Tabs 示例</div>
+  <h1>示例1</h1>
+  <Tabs>
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+  </Tabs>
 </template>
 
 <script lang="ts">
+import Tabs from '../lib/Tabs.vue';
+import Tab from '../lib/Tab.vue';
 
+export default {
+  components: {Tabs, Tab}
+};
 </script>
 
 <style lang="scss" scoped>
