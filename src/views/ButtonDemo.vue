9d1677d526acf156903e7d4678c574c6fdf25d0a
lu <919041098@qq.com>
2021-03-21 09:56:32 +0800

    创建Button组件

--- src/views/ButtonDemo.vue
+++ src/views/ButtonDemo.vue
@@ -1,109 +1,17 @@
 <template>
-Switch 的内容
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
-  <p>Switch content</p>
+  <div>Button 示例</div>
+  <h1>示例一</h1>
+  <div>
+    <Button>按钮</Button>
+  </div>
 </template>
 
 <script lang="ts">
+import Button from '../lib/Button.vue';
 
+export default {
+  components: {Button}
+};
 </script>
 
 <style lang="scss" scoped>
