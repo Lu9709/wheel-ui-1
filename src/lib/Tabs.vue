6cadb47c60258faee6962f7721913a842b7d318f
lu <919041098@qq.com>
2021-03-21 19:28:54 +0800

    渲染嵌套插槽内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
-    Tabs 组件
-    <component :is="defaults[0]"></component>
-    <component :is="defaults[1]"></component>
+    <div v-for="(t,index) in titles" :key="index">{{t}}</div>
+    <component v-for="(c,index) in defaults" :key="index" :is="c"></component>
   </div>
 </template>
 <script lang="ts">
 import Tab from  './Tab.vue'
 export default {
   setup(props, context) {
-    console.log(...context.slots.default());
     const defaults = context.slots.default();
     defaults.forEach((tag)=>{
       if(tag.type !== Tab){
         throw new Error('子组件必须是Tab')
       }
     })
-    return {defaults}
+    const titles = defaults.map((tag)=>{
+      return tag.props.title
+    })
+    return {defaults,titles}
   }
 };
 </script>
