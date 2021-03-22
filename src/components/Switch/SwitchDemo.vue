301eb3e6a3bca85f6e0775cffad33ad9f9673264
lu <919041098@qq.com>
2021-03-22 22:48:41 +0800

    解决了button disabled的问题

--- src/components/Switch/SwitchDemo.vue
+++ src/components/Switch/SwitchDemo.vue
@@ -1,11 +1,15 @@
 <template>
-  <Demo :component="Switch1Demo"/>
-  <Demo :component="Switch2Demo"/>
+  <div>
+    <h1>Switch 组件示例</h1>
+    <Demo :component="Switch1Demo"/>
+    <Demo :component="Switch2Demo"/>
+  </div>
 </template>
 <script lang="ts">
-import Switch1Demo from './Switch1.demo.vue'
-import Switch2Demo from './Switch2.demo.vue'
-import Demo from '../Demo.vue'
+import Switch1Demo from './Switch1.demo.vue';
+import Switch2Demo from './Switch2.demo.vue';
+import Demo from '../Demo.vue';
+
 export default {
   components: {Demo},
   setup() {
@@ -16,24 +20,30 @@ export default {
 
 <style lang="scss" scoped>
 $border-color: #d9d9d9;
+
 .demo {
   border: 1px solid $border-color;
   margin: 16px 0 32px;
+
   >h2 {
     font-size: 20px;
     padding: 8px 16px;
     border-bottom: 1px solid $border-color;
   }
+
   &-component {
     padding: 16px;
   }
+
   &-actions {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
   }
+
   &-code {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
+
     >pre {
       line-height: 1.1;
       font-family: Consolas, 'Courier New', Courier, monospace;
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -5,29 +5,29 @@
 <script lang="ts">
 
 export default {
-  props:{
-    value:Boolean
+  props: {
+    value: Boolean
   },
-  setup(props,content) {
-    const toggle =()=>{
-      content.emit('update:input',!props.value)
-    }
-    return {toggle}
+  setup(props, context) {
+    const toggle = () => {
+      context.emit('update:value', !props.value);
+    };
+    return {toggle};
   }
 };
 </script>
 
-<style lang="scss" >
+<style lang="scss">
 $h: 22px;
 $h2: $h - 4px;
 .gulu-switch {
   height: $h;
-  width: $h*2;
+  width: $h * 2;
   border: none;
   background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
-  transition: all 250ms;
+
   > span {
     position: absolute;
     top: 2px;
@@ -36,23 +36,33 @@ $h2: $h - 4px;
     width: $h2;
     background: white;
     border-radius: $h2 / 2;
+    transition: all 250ms;
   }
+
   &.gulu-checked {
     background: #1890ff;
+
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
   }
-  &:hover > span {
-    left: calc(100% - #{$h2} - 2px);
-  }
+
   &:focus {
     outline: none;
   }
-  &:active{
-    > span {width: $h2 + 4px;}
+
+  &:active {
+    > span {
+      width: $h2 + 4px;
+    }
   }
-  &.gulu-checked:active{
-    > span {width: $h2 + 4px; margin-left: -4px;}
+
+  &.gulu-checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
   }
 }
-
 
 </style>
